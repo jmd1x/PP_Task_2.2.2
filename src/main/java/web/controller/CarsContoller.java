129fb05674381c42f75ebbc280af13808e51259e
jmd1x <144138299+jmd1x@users.noreply.github.com>
2023-11-09 15:30:06 +0300

    Version 1.0

--- src/main/java/web/controller/CarsContoller.java
+++ src/main/java/web/controller/CarsContoller.java
@@ -2,7 +2,7 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
@@ -13,7 +13,7 @@ public class CarsContoller {
     private CarService carService;
 
     @GetMapping("/cars")
-    public String printCars(@RequestParam(value = "count", defaultValue = "5") Integer n, ModelMap model) {
+    public String printCars(@RequestParam(value = "count", defaultValue = "5") Integer n, Model model) {
         model.addAttribute("cars", carService.getCars(n));
         return "cars";
     }
